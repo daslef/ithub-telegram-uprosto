40b53d86164210e794bcec641c92484063e73714
BYBLIK-C-MALINOY <morty0472@gmail.com>
2025-07-04 17:31:55 +0300

    pdf

---
+++ backend/pdf.py
@@ -0,0 +1,48 @@
+import mysql.connector
+from fpdf import FPDF
+
+def fetch_data():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="bot_db_local"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT category_name, category_description FROM `categories`")
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+
+def create_pdf(data, filename="export.pdf"):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.add_font(fname='DejaVuSans.ttf')
+    pdf.set_font('DejaVuSans', size=14)
+    pdf.set_text_shaping(True)
+
+
+    pdf.cell(0, 10, "Экспорт данных из SQL", align="C")
+
+    for row in data:
+        for item in row:
+            pdf.cell(40, 10, str(item), border=1)
+        pdf.ln() 
+
+    pdf.output(filename)
+    return filename
+
+create_pdf(fetch_data())
+# from telegram import Bot
+# import os
+
+#     async def send_pdf(chat_id, bot_token):
+#         bot = Bot(token=bot_token)
+#         data = fetch_data()  
+#         pdf_path = create_pdf(data)  
+
+#         with open(pdf_path, "rb") as file:
+#             await bot.send_document(chat_id=chat_id, document=file)
+
+#     os.remove(pdf_path)
